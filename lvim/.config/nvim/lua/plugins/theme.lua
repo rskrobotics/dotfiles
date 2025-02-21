0408ecf8987d3c9938256f4bb53a89a97b59de9d
Krzysztof Kasprzyk <rskrobotics@gmail.com>
2025-02-21 12:49:34 +0100

    add new lazyvim config

---
+++ lvim/.config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
