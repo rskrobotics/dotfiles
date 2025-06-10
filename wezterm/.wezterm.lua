ab0dda87c4974257bdb263853c60775cf1e5afce
Krzysztof Kasprzyk <rskrobotics@gmail.com>
2025-06-10 14:15:51 +0200

    add zsh config and wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,14 +1,14 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("JetBrainsMono Nerd Font"),
+	font = wezterm.font("FiraCode Nerd Font"),
 	font_size = 14.0,
 
-	color_scheme = "Catppuccin Mocha", -- or Dracula, etc.
-	window_background_opacity = 0.85,
+	color_scheme = "Dracula (Official)",
+	window_background_opacity = 0.7,
 	macos_window_background_blur = 20,
 
-	window_decorations = "RESIZE", -- No titlebar, but resizeable
+	window_decorations = "RESIZE",
 	use_fancy_tab_bar = false,
 	hide_tab_bar_if_only_one_tab = true,
 
@@ -18,4 +18,7 @@ return {
 		top = 20,
 		bottom = 20,
 	},
+
+	-- Enable font ligatures for Fira Code's special programming symbols
+	font_ligatures = "enable",
 }
